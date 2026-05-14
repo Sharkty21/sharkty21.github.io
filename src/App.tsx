99c416627959bf00454df61725d3fa213f723793
Sharkty21 <tylermsharkey@gmail.com>
2026-05-14 16:44:32 -0500

    url fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { Switch, Route, Router as WouterRouter } from "wouter";
+import { Switch, Route, Redirect } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 
 const queryClient = new QueryClient();
@@ -11,7 +10,12 @@ function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route component={NotFound} />
+      <Route path="/TylerSharkeyPortfolio">
+        <Redirect to="/" />
+      </Route>
+      <Route>
+        <Redirect to="/" />
+      </Route>
     </Switch>
   );
 }
@@ -20,9 +24,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
-          <Router />
-        </WouterRouter>
+        <Router />
         <Toaster />
       </TooltipProvider>
     </QueryClientProvider>
